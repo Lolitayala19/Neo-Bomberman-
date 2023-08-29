b3ddb6735b1e0819afc23eeb793eb2b8d540d7e7
Eduardo Correa Flores <143230099+Lolitayala19@users.noreply.github.com>
2023-08-29 01:53:31 +0000

    iostream

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+
+
+int main(int argc, char const *argv[])
+{
+
+    std::cout<<"Mensaje"<<std::endl;
+    for (int i = 0; i < 10; i++)
+    {
+
+        /* code */
+    }
+
+    /* code */
+    return 0;
+}
